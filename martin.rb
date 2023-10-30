e6c60894b2c823406092385e83b49a432c739be3
Yuri Astrakhan <yuriastrakhan@gmail.com>
2023-10-30 17:30:24 -0400

    v0.10

--- martin.rb
+++ martin.rb
@@ -4,29 +4,29 @@
 #
 
 class Martin < Formula
-  current_version="0.9.3"
+  current_version="0.10.0"
 
   desc "Blazing fast and lightweight tile server with PostGIS, MBTiles, and PMTiles support, plus an mbtiles tool"
   homepage "https://github.com/maplibre/martin"
 
   on_macos do
     on_arm do
-      sha256 "46f4f4ca2f299ae23431fe030de647b6f93697ca0f1f9ea0bcba0bdbc4d73ea3}"
+      sha256 "0d7645264af4ab7c412d8a3b17d29e2d172dd89b444827b94ef503f8900c86f2}"
       url "https://github.com/maplibre/martin/releases/download/v#{current_version}/martin-Darwin-aarch64.tar.gz"
     end
     on_intel do
-      sha256 "c0c3069a1ee96f30c1fe31112b8ec7ceed5cadb04a0dce449f5aee54de7e8f4f"
+      sha256 "f2e6a8689515a89793d2de430e20d4781eb0b46e2b7953b6f740a45c4d3d4e21"
       url "https://github.com/maplibre/martin/releases/download/v#{current_version}/martin-Darwin-x86_64.tar.gz"
     end
   end
 
   on_linux do
     on_arm do
-      sha256 "78790b1f94bfeb644cff800c8daa844159934fe471ae7c09b1e8543f0fc6dcb0"
+      sha256 "9a0c0db852914dcbd19c37c3b81233dab886957a502b0cb77c8b3638895f31ed"
       url "https://github.com/maplibre/martin/releases/download/v#{current_version}/martin-Linux-aarch64-musl.tar.gz"
     end
     on_intel do
-      sha256 "b017c64bfc5f55fe9869510b91b357af47e497c60422201cfb197a6caa100e9a"
+      sha256 "3ebce6b325ff61c19660fde41bf5097f76976bdfee0e44294b706a5e7b8cc69d"
       url "https://github.com/maplibre/martin/releases/download/v#{current_version}/martin-Linux-x86_64-musl.tar.gz"
     end
   end
